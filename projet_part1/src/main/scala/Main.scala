8c6a9b035a29cdb60f9ac45bcb6e75608673fc9f
Morgan <94167067+TeyKra@users.noreply.github.com>
2023-12-15 16:57:28 +0100

    Update

--- projet_part1/src/main/scala/Main.scala
+++ projet_part1/src/main/scala/Main.scala
@@ -1,5 +1,5 @@
 object Main extends App {
-  val cheminFichierCSV = "chemin/vers/le/fichier/Tigres.csv" // Remplacez par le chemin réel de votre fichier CSV
+  val cheminFichierCSV = "chemin/vers/le/fichier/Tigres.csv" 
   val tigres = ParseurCSV.parseCSV(cheminFichierCSV)
 
   tigres.foreach { tigre =>
--- projet_part1/src/main/scala/service/ParseurCSV.scala
+++ projet_part1/src/main/scala/service/ParseurCSV.scala
@@ -3,7 +3,7 @@ import scala.util.Try
 
 object ParseurCSV {
   def parseCSV(filePath: String): List[Tigre] = {
-    val lines = Source.fromFile(filePath).getLines().drop(1) // Skip header
+    val lines = Source.fromFile(filePath).getLines().drop(1) 
     lines.map { line =>
       val fields = line.split(",").map(_.trim)
       Tigre(
